2e6def63dbf88f9c4be20670975f184de7637c6a
LidoHon <lidohon10@gmail.com>
2024-10-08 13:50:46 +0300

    modify migration file

--- hasura/migrations/default/1728378138762_sync-existing-schema/up.sql
+++ hasura/migrations/default/1728378138762_sync-existing-schema/up.sql
@@ -1,15 +1,28 @@
 SET check_function_bodies = false;
-CREATE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
+
+DO $$
+BEGIN
+  -- Check if the function already exists
+  IF NOT EXISTS (
+    SELECT 1 
+    FROM pg_proc 
+    WHERE proname = 'set_current_timestamp_updated_at'
+  ) THEN
+    -- Create the function only if it doesn't exist
+    CREATE FUNCTION public.set_current_timestamp_updated_at() RETURNS trigger
     LANGUAGE plpgsql
     AS $$
-DECLARE
-  _new record;
-BEGIN
-  _new := NEW;
-  _new."updated_at" = NOW();
-  RETURN _new;
-END;
-$$;
+    DECLARE
+      _new record;
+    BEGIN
+      _new := NEW;
+      _new."updated_at" = NOW();
+      RETURN _new;
+    END;
+    $$;
+  END IF;
+END $$;
+
 CREATE TABLE public.friend (
     id integer NOT NULL,
     username text,
@@ -23,6 +36,7 @@ CREATE SEQUENCE public.friend_id_seq
     NO MAXVALUE
     CACHE 1;
 ALTER SEQUENCE public.friend_id_seq OWNED BY public.friend.id;
+
 CREATE TABLE public.pizza (
     id integer NOT NULL,
     title integer NOT NULL,
@@ -38,6 +52,7 @@ CREATE SEQUENCE public.pizza_id_seq
     NO MAXVALUE
     CACHE 1;
 ALTER SEQUENCE public.pizza_id_seq OWNED BY public.pizza.id;
+
 CREATE TABLE public.pizza_order (
     id integer NOT NULL,
     friend_id integer NOT NULL,
@@ -51,6 +66,7 @@ CREATE SEQUENCE public.pizza_order_id_seq
     NO MAXVALUE
     CACHE 1;
 ALTER SEQUENCE public.pizza_order_id_seq OWNED BY public.pizza_order.id;
+
 CREATE TABLE public.pizza_topping (
     id integer NOT NULL,
     title text NOT NULL,
@@ -65,6 +81,7 @@ CREATE SEQUENCE public.pizza_topping_id_seq
     NO MAXVALUE
     CACHE 1;
 ALTER SEQUENCE public.pizza_topping_id_seq OWNED BY public.pizza_topping.id;
+
 CREATE TABLE public.pizza_topping_pizza (
     id integer NOT NULL,
     pizza_id integer NOT NULL,
@@ -78,11 +95,13 @@ CREATE SEQUENCE public.pizza_topping_pizza_id_seq
     NO MAXVALUE
     CACHE 1;
 ALTER SEQUENCE public.pizza_topping_pizza_id_seq OWNED BY public.pizza_topping_pizza.id;
+
 ALTER TABLE ONLY public.friend ALTER COLUMN id SET DEFAULT nextval('public.friend_id_seq'::regclass);
 ALTER TABLE ONLY public.pizza ALTER COLUMN id SET DEFAULT nextval('public.pizza_id_seq'::regclass);
 ALTER TABLE ONLY public.pizza_order ALTER COLUMN id SET DEFAULT nextval('public.pizza_order_id_seq'::regclass);
 ALTER TABLE ONLY public.pizza_topping ALTER COLUMN id SET DEFAULT nextval('public.pizza_topping_id_seq'::regclass);
 ALTER TABLE ONLY public.pizza_topping_pizza ALTER COLUMN id SET DEFAULT nextval('public.pizza_topping_pizza_id_seq'::regclass);
+
 ALTER TABLE ONLY public.friend
     ADD CONSTRAINT friend_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.friend
@@ -95,8 +114,10 @@ ALTER TABLE ONLY public.pizza_topping_pizza
     ADD CONSTRAINT pizza_topping_pizza_pkey PRIMARY KEY (id);
 ALTER TABLE ONLY public.pizza_topping
     ADD CONSTRAINT pizza_topping_pkey PRIMARY KEY (id);
+
 CREATE TRIGGER set_public_pizza_updated_at BEFORE UPDATE ON public.pizza FOR EACH ROW EXECUTE FUNCTION public.set_current_timestamp_updated_at();
 COMMENT ON TRIGGER set_public_pizza_updated_at ON public.pizza IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+
 ALTER TABLE ONLY public.pizza_order
     ADD CONSTRAINT friend_pizza_order FOREIGN KEY (friend_id) REFERENCES public.friend(id) ON DELETE CASCADE;
 ALTER TABLE ONLY public.pizza_order
